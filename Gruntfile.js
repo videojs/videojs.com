f1efa80e45c722ed32c7376e1de94e6fd9635e93
Gary Katsevman <git@gkatsev.com>
2016-05-02 19:32:07 -0400

    Initial commit for advanced example page

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,6 +10,10 @@ module.exports = function(grunt) {
           spawn: false,
         },
       },
+      vendor: {
+        files: ['./node_modules/videojs-playlist-ui/dist/*'],
+        tasks: ['copy:vendor']
+      }
     },
     concurrent: {
       options: {
@@ -34,19 +38,31 @@ module.exports = function(grunt) {
         files: {
           '_harp/js/index.js': ['_src-js/index.js'],
           '_harp/js/home.js': ['_src-js/home.js'],
+          '_harp/js/advanced.js': ['_src-js/advanced.js'],
           '_harp/js/plugins.js': ['_src-js/plugins.js'],
           '_harp/js/support.js': ['_src-js/support.js']
         },
         options: {
-          external: ['videojsSite'],
           transform: [
             require('babelify').configure({}),
-            "browserify-shim"
+            [require('browserify-global-shim').configure({
+              'video.js': 'videojs'
+            }), {global: true}]
           ]
         }
       }
     },
     copy: {
+      vendor: {
+        files: [
+          {
+            expand: true,
+            src: 'node_modules/videojs-playlist-ui/dist/*',
+            dest: '_harp/vendor/videojs-playlist-ui/',
+            flatten: true
+          }
+        ]
+      },
       dist: {
         files: [
           {
@@ -100,7 +116,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-copy');
 
   // Default task(s).
-  grunt.registerTask('default', ['copy:fontawesome', 'copy:stylevjs', 'copy:fontvjs', 'browserify', 'harp:dist']);
+  grunt.registerTask('default', ['copy:fontawesome', 'copy:vendor', 'copy:stylevjs', 'copy:fontvjs', 'browserify', 'harp:dist']);
   grunt.registerTask('dist', ['default', 'copy:dist']);
   grunt.registerTask('dev', ['browserify', 'concurrent']); // Browserify before starting concurrent things
 };
