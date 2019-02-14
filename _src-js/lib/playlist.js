9ca55970342fdae3c75e81f5cd33f079f45b94a3
Gary Katsevman <git@gkatsev.com>
2019-02-14 15:44:59 -0500

    update poster and duration for bipbop (#103)

--- _src-js/lib/playlist.js
+++ _src-js/lib/playlist.js
@@ -57,7 +57,19 @@ export default [{
       src: '//d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8',
       type: 'application/x-mpegurl'
     }
-  ]
+  ],
+  poster: '//d2zihajmogu5jn.cloudfront.net/bipbop-advanced/poster.png',
+  thumbnail: [
+    {
+      srcset: '//d2zihajmogu5jn.cloudfront.net/bipbop-advanced/poster.png',
+      type: 'image/png',
+      media: '(min-width: 400px;)'
+    },
+    {
+      src: '//d2zihajmogu5jn.cloudfront.net/bipbop-advanced/poster.png'
+    }
+  ],
+  duration: 1800
 }, {
   name: "Elephant's Dream (HLS with captions, audio description and chapters)",
   description: 'The film features two men, Proog, who is older and more experienced, and Emo, who is young and nervous, living in a miraculous construction referred to only as "The Machine".',
