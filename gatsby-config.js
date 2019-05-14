b31ffa9068c598a24af0dbb82c45fd677f1c8390
Matthew McClure <m@mmcc.io>
2019-05-13 21:08:38 -0700

    started oli design

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Video.js`,
+    description: `Make your player yours with the internet's most popular video player`,
+    author: `@videojs`,
   },
   plugins: [
+    'gatsby-plugin-styled-components',
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
