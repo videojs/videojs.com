869e5b5e14b8004f65b4088c4f30b6993659612c
Matthew McClure <m@mmcc.io>
2019-06-24 10:28:24 -0700

    saving progress

---
+++ src/components/Code.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Code = styled.code`
+  background-color: rgb(247, 249, 250);
+  color: rgb(56, 65, 74);
+  padding: 0.25em;
+
+  border: 1px solid rgb(230, 232, 235);
+`;
+
+export default Code;
