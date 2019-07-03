54b5a555745028c56aa440cc66d67bbd53137c77
meyginix <meyginix@gmail.com>
2019-07-03 16:25:10 +0300

    Refactoring

--- src/components/HomeComponents/AdvancedExample/index.jsx
+++ src/components/HomeComponents/AdvancedExample/index.jsx
@@ -13,6 +13,7 @@ import MediaEventItem from './MediaEventItem';
 import MediaProperties from './mediaProperties';
 import MediaEvents from './mediaEvents';
 import PlayerControls from './PlayerControls';
+import PlayerPlaylist from './PlayerPlaylist';
 import playlist from './playlist';
 import rectangles from '../../../images/footer-rectangles.svg';
 import { media } from '../../../utils/styles';
@@ -20,55 +21,39 @@ import { media } from '../../../utils/styles';
 import 'video.js/dist/video-js.css';
 import 'videojs-playlist-ui/dist/videojs-playlist-ui.css';
 
-const Description = styled.div`
+const PlaylistPluginDescContainer = styled(Container)`
   display: flex;
-  justify-content: space-between;
-  margin-bottom: 1.5em;
-`;
-
-const TestItOutWrapper = styled.div`
-  position: relative;
-  flex-basis: 67%;
-  display: flex;
+  flex-direction: column;
   align-items: flex-end;
-  color: rgba(0, 0, 0, 0.3);
-  font-size: 12px;
-  font-weight: bold;
-  text-transform: uppercase;
+  margin-bottom: 2.2em;
 
-  p {
-    position: absolute;
-    left: 0;
+  > * {
+    width: 33%;
 
-    ${media.medLarge`
-      left: -2.6em;
-    `}
-
-    ${media.wide`
-      left: -8.2em;
-    `}
+    &:not(:last-child) {
+      margin-bottom: 1.5em;
+    }
   }
 `;
 
-const SectionHeaderWrapper = styled.div`
-  flex-basis: 33%;
+const StyledSectionHeader = styled(SectionHeader)`
+  margin: 0;
 `;
 
-const P = styled.p`
+const PlaylistPluginDesc = styled.p`
   color: rgba(0, 0, 0, 0.6);
   font-size: 14px;
   font-weight: 300;
   line-height: 2.14;
   opacity: 0.6;
-  margin-bottom: 1em;
 `;
 
-const Wrapper = styled.div`
+const PlayerBackground = styled.div`
   width: 100%;
   background-image: url(${rectangles});
 `;
 
-const StyledContainer = styled(Container)`
+const PlayerContainer = styled(Container)`
   ${media.medLarge`
     padding-top: 0;
     padding-bottom: 0;
@@ -79,7 +64,7 @@ const ControlsWrapper = styled.div`
   position: relative;
 `;
 
-const PlayerWrapper = styled.div`
+const VideoWrapper = styled.div`
   display: flex;
   flex-wrap: wrap;
   justify-content: space-between;
@@ -92,87 +77,6 @@ const Video = styled.video`
   min-height: 150px;
   height: 0;
   position: relative;
-`;
-
-const PlaylistWrapper = styled.div`
-  flex: 1 1 25%;
-  position: relative;
-
-  .vjs-playlist {
-    background: transparent;
-    position: absolute;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    margin: 0;
-    height: 100%;
-
-    .vjs-playlist-item {
-      margin-left: 1em;
-      height: 9em;
-      outline: none;
-
-      &:not(:first-child) {
-        margin-top: 1em;
-      }
-
-      &.vjs-selected {
-        background: transparent;
-
-        img {
-          opacity: 1;
-        }
-
-        .vjs-playlist-now-playing-text {
-          background-color: #000;
-          border-radius: 0.5em;
-          color: #fff;
-          font-size: 13px;
-          font-weight: bold;
-          top: 1em;
-          left: 1.5em;
-          margin: 0;
-          padding: 0.2em 0.8em;
-        }
-      }
-
-      .vjs-playlist-title-container {
-        padding: 1em 1.2em;
-      }
-
-      .vjs-up-next-text,
-      .vjs-playlist-name {
-        font-size: 13px;
-        font-weight: bold;
-        padding: 0;
-      }
-
-      .vjs-up-next-text {
-        text-transform: none;
-      }
-
-      .vjs-playlist-duration {
-        top: 1em;
-        left: 1.5em;
-        background-color: #fff;
-        border-radius: 0.5em;
-        color: #000;
-        font-size: 13px;
-        font-weight: bold;
-        padding: 0.2em 0.8em;
-      }
-
-      picture {
-        height: 100%;
-      }
-
-      img {
-        height: 100%;
-        object-fit: cover;
-      }
-    }
-  }
 `;
 
 const MediaInfoWrapper = styled.div`
@@ -227,25 +131,23 @@ class AdvancedExample extends React.Component {
 
   render () {
     return (
-      <div>
-        <Container>
-          <Description>
-            <TestItOutWrapper><p>test it out</p></TestItOutWrapper>
-            <SectionHeaderWrapper>
-              <SectionHeader left title="Example" tagline="Playlist plugin" />
-              <P>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-              </P>
-            </SectionHeaderWrapper>
-          </Description>
-        </Container>
-        <Wrapper>
-          <StyledContainer>
+      <>
+        <PlaylistPluginDescContainer>
+          <StyledSectionHeader left title="Example" tagline="Playlist plugin" />
+          <PlaylistPluginDesc>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+          </PlaylistPluginDesc>
+        </PlaylistPluginDescContainer>
+        <PlayerBackground>
+          <PlayerContainer>
             <ControlsWrapper>
               {this.state.isPlayerInitialized && (
                 <PlayerControls player={this.player} />
               )}
-              <PlayerWrapper>
+              <VideoWrapper>
                 <Video
                   ref={(el) => { this.videoEl = el }}
                   controls
@@ -254,10 +156,8 @@ class AdvancedExample extends React.Component {
                   crossOrigin="anonymous"
                   className="video-js vjs-fluid"
                 />
-                <PlaylistWrapper>
-                  <div className="vjs-playlist vjs-fluid" />
-                </PlaylistWrapper>
-              </PlayerWrapper>
+                <PlayerPlaylist />
+              </VideoWrapper>
             </ControlsWrapper>
             {this.state.isPlayerInitialized && (
               <MediaInfoWrapper>
@@ -273,9 +173,9 @@ class AdvancedExample extends React.Component {
                 />
               </MediaInfoWrapper>
             )}
-          </StyledContainer>
-        </Wrapper>
-      </div>
+          </PlayerContainer>
+        </PlayerBackground>
+      </>
     );
   }
 }
