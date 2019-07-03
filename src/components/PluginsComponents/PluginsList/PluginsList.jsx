446ef59ce1be9bfce26cd3f8d48203bf09e837c1
meyginix <meyginix@gmail.com>
2019-07-03 14:03:39 +0300

    Plugins list is done

--- src/components/PluginsComponents/PluginsList/PluginsList.jsx
+++ src/components/PluginsComponents/PluginsList/PluginsList.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
+import shortid from 'shortid';
+
+import Container from '../../Container';
+import PluginsListItem from './PluginsListItem';
+
+const Wrapper = styled.div`
+  margin: 3.5em 0 10em;
+`;
 
 const PluginsList = ({ plugins }) => (
-  <ul>
-    {plugins.map(p => <li>{p.name}</li>)}
-  </ul>
+  <Container>
+    <Wrapper>
+      {plugins.map((plugin) => (
+        <PluginsListItem key={shortid.generate()} {...plugin} />
+      ))}
+    </Wrapper>
+  </Container>
 );
 
 export default PluginsList;
