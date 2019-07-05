4c3fbd1ae111aa779039ebffaa27355c341765f9
meyginix <meyginix@gmail.com>
2019-07-05 18:20:56 +0300

    Blog tags

---
+++ src/components/BlogComponents/BlogSidebarBlock.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import styled from 'styled-components';
+import shortid from 'shortid';
+import { Link } from 'gatsby';
+
+const Wrapper = styled.div`
+  border: 2px solid #ebebeb;
+  margin-bottom: 3.3em;
+`;
+
+const Heading = styled.h4`
+  background-color: #f9f9f9;
+  border-bottom: 2px solid #ebebeb;
+  display: block;
+  color: rgba(0, 0, 0, 0.3);
+  font-size: 16px;
+  padding: 1em 0;
+  text-align: center;
+  text-transform: uppercase;
+`;
+
+const Links = styled.div`
+  color: rgba(0, 0, 0, 0.6);
+  font-weight: 300;
+  line-height: 1.88;
+  padding: 2em;
+
+  a {
+    display: block;
+    color: inherit;
+    text-decoration: underline;
+
+    &:not(:last-child) {
+      margin-bottom: 1em;
+    }
+  }
+`;
+
+const BlogSidebarBlock = ({ heading, items }) => (
+  <Wrapper>
+    <Heading>{heading}</Heading>
+    <Links>
+      {items.map(({ url, label }) => (
+        <Link key={shortid.generate()} to={url}>
+          {label}
+        </Link>
+      ))}
+    </Links>
+  </Wrapper>
+);
+
+export default BlogSidebarBlock;
