5009767b6bc7f4307d298f37ba32c5dc5b307f7c
meyginix <meyginix@gmail.com>
2019-08-06 15:32:31 +0300

    Small fixes for logos

--- src/images/logos/consumerReports.jsx
+++ src/images/logos/consumerReports.jsx
@@ -10,16 +10,16 @@ const ConsumerReportsLogo = (props) => (
     {...props}
   >
     <style>
-      {'.prefix__st3{fill-rule:evenodd;clip-rule:evenodd;fill:#b2b2b2}'}
+      {'.consumer-reports-logo__st3{fill-rule:evenodd;clip-rule:evenodd;fill:#b2b2b2}'}
     </style>
     <title>{'Page 1'}</title>
-    <g id="prefix__Symbols">
-      <g id="prefix__CR-StackedLogo">
-        <g id="prefix__Page-1">
-          <g id="prefix__Group-3" transform="translate(0 .178)">
+    <g id="consumer-reports-logo__Symbols">
+      <g id="consumer-reports-logo__CR-StackedLogo">
+        <g id="consumer-reports-logo__Page-1">
+          <g id="consumer-reports-logo__Group-3" transform="translate(0 .178)">
             <defs>
               <filter
-                id="prefix__Adobe_OpacityMaskFilter"
+                id="consumer-reports-logo__Adobe_OpacityMaskFilter"
                 filterUnits="userSpaceOnUse"
                 x={0}
                 y={0}
@@ -35,11 +35,11 @@ const ConsumerReportsLogo = (props) => (
               y={0}
               width={57}
               height={34.8}
-              id="prefix__mask-2_1_"
+              id="consumer-reports-logo__mask-2_1_"
             >
-              <g filter="url(#prefix__Adobe_OpacityMaskFilter)">
+              <g filter="url(#consumer-reports-logo__Adobe_OpacityMaskFilter)">
                 <path
-                  id="prefix__path-1_1_"
+                  id="consumer-reports-logo__path-1_1_"
                   fillRule="evenodd"
                   clipRule="evenodd"
                   fill="#fff"
@@ -48,87 +48,87 @@ const ConsumerReportsLogo = (props) => (
               </g>
             </mask>
             <path
-              id="prefix__Fill-1"
+              id="consumer-reports-logo__Fill-1"
               d="M38.1 15.9V8.1h4.7c2.2 0 4 1.8 4 4S45 16 42.8 16h-4.7zm-14.5 7.6c-1.6 1.6-3.8 2.6-6.2 2.6-4.8 0-8.7-3.9-8.7-8.7 0-4.8 3.9-8.7 8.7-8.7 2.4 0 4.6 1 6.2 2.6l5.9-5.9v24l-5.9-5.9zm5.8 10.8h8.7V23.5h2.6L47 34.3h10l-7.6-12.4c3.6-1.9 6.1-5.6 6.1-10C55.5 5.6 50.4.6 44.2.6H29.4V5c-3.1-3.1-7.3-5-12-5C7.8 0 0 7.7 0 17.4s7.8 17.4 17.4 17.4c4.7 0 8.9-1.9 12.1-4.9v4.4z"
-              mask="url(#prefix__mask-2_1_)"
+              mask="url(#consumer-reports-logo__mask-2_1_)"
               fillRule="evenodd"
               clipRule="evenodd"
               fill="#b2b2b2"
             />
           </g>
           <path
-            id="prefix__Fill-4"
-            className="prefix__st3"
+            id="consumer-reports-logo__Fill-4"
+            className="consumer-reports-logo__st3"
             d="M150.7 16.1V9.9c.2-2.3 2.2-3.5 4.2-3.2V5.5c-1.8-.3-3.4.6-4.2 2.2V5.6h-1.1v10.6h1.1z"
           />
           <path
-            id="prefix__Fill-6"
-            className="prefix__st3"
+            id="consumer-reports-logo__Fill-6"
+            className="consumer-reports-logo__st3"
             d="M139.1 10c.3-2.1 1.9-3.6 3.9-3.6 1.9 0 3.5 1.4 3.7 3.6h-7.6zm8.7.4c0-3-2.2-4.9-4.8-4.9-2.8 0-5.1 2.2-5.1 5.4 0 3.1 2.4 5.4 5.3 5.4 1.7 0 3.2-.7 4.2-2l-.8-.7c-.8 1.1-1.9 1.6-3.3 1.6-2.2 0-3.9-1.7-4.1-4h8.7v-.8z"
           />
           <path
-            id="prefix__Fill-8"
-            className="prefix__st3"
+            id="consumer-reports-logo__Fill-8"
+            className="consumer-reports-logo__st3"
             d="M121.6 10.7c0-2.8 1.5-4.3 3.4-4.3 1.7 0 2.8 1.1 2.7 3v6.7h1.1v-5.4c0-2.8 1.5-4.3 3.4-4.3 1.7 0 2.8 1.1 2.7 3v6.7h1.1V9.4c0-2.5-1.4-4-3.7-4-1.7 0-3.1 1-3.7 2.5-.5-1.6-1.7-2.5-3.5-2.5-1.6 0-2.9.9-3.5 2.2V5.5h-1.1v10.6h1.1v-5.4z"
           />
           <path
-            id="prefix__Fill-10"
-            className="prefix__st3"
+            id="consumer-reports-logo__Fill-10"
+            className="consumer-reports-logo__st3"
             d="M117.2 16.1h1.1V5.5h-1.1v5.4c0 2.8-1.6 4.3-3.7 4.3-1.7 0-2.8-1.1-2.8-3V5.5h-1.1v6.7c0 2.5 1.4 4 3.8 4 1.7 0 3.2-.9 3.9-2.3v2.2z"
           />
           <path
-            id="prefix__Fill-12"
-            className="prefix__st3"
+            id="consumer-reports-logo__Fill-12"
+            className="consumer-reports-logo__st3"
             d="M100.8 13.9l-.6.9c1 .9 2.3 1.5 4 1.5 2 0 3.6-1.1 3.6-3 0-1.5-.7-2.1-3.1-3l-.9-.3c-1.3-.4-2-.9-2-1.9 0-1.2 1.1-1.7 2.2-1.7 1 0 2 .5 2.8 1.2l.7-.9c-1-.8-2.1-1.3-3.5-1.3-1.8 0-3.3 1-3.3 2.8 0 1.4.8 2.2 2.8 2.9l1 .4c1.5.5 2.2.8 2.2 1.8 0 1.4-1.2 2-2.4 2-1.5-.1-2.6-.6-3.5-1.4"
           />
           <path
-            id="prefix__Fill-14"
-            className="prefix__st3"
+            id="consumer-reports-logo__Fill-14"
+            className="consumer-reports-logo__st3"
             d="M90.9 10.7c0-2.8 1.6-4.3 3.7-4.3 1.7 0 2.8 1.1 2.8 3v6.7h1.1V9.4c0-2.5-1.4-4-3.8-4-1.7 0-3.2.9-3.9 2.3V5.5h-1.1v10.6h1.1v-5.4z"
           />
           <path
-            id="prefix__Fill-16"
-            className="prefix__st3"
+            id="consumer-reports-logo__Fill-16"
+            className="consumer-reports-logo__st3"
             d="M82.6 15.1c-2.3 0-4.2-1.9-4.2-4.3s1.9-4.3 4.2-4.3c2.4 0 4.3 1.9 4.3 4.3s-2 4.3-4.3 4.3m0-9.7c-3 0-5.4 2.4-5.4 5.4s2.4 5.4 5.4 5.4c3 0 5.4-2.4 5.4-5.4s-2.4-5.4-5.4-5.4"
           />
           <path
-            id="prefix__Fill-18"
-            className="prefix__st3"
+            id="consumer-reports-logo__Fill-18"
+            className="consumer-reports-logo__st3"
             d="M75 12.4C73.8 14 71.9 15 69.8 15c-3.6 0-6.6-3-6.6-6.6 0-3.6 3-6.6 6.6-6.6 2.1 0 4 1 5.2 2.6l.9-.9C74.4 1.7 72.2.6 69.8.6 65.5.6 62 4.1 62 8.4s3.5 7.8 7.8 7.8c2.5 0 4.7-1.1 6.1-2.9l-.9-.9z"
           />
           <path
-            id="prefix__Fill-20"
-            className="prefix__st3"
+            id="consumer-reports-logo__Fill-20"
+            className="consumer-reports-logo__st3"
             d="M124.1 32.2l-.6.9c1 .9 2.3 1.5 4 1.5 2 0 3.6-1.1 3.6-3 0-1.5-.7-2.1-3.1-3l-.9-.3c-1.3-.4-2-.9-2-1.9 0-1.2 1.1-1.7 2.2-1.7 1 0 2 .5 2.8 1.2l.7-.9c-1-.8-2.1-1.3-3.5-1.3-1.8 0-3.3 1-3.3 2.8 0 1.4.8 2.2 2.8 2.9l1 .4c1.5.5 2.2.8 2.2 1.8 0 1.4-1.2 2-2.4 2-1.5-.1-2.6-.6-3.5-1.4"
           />
           <path
-            id="prefix__Fill-22"
-            className="prefix__st3"
+            id="consumer-reports-logo__Fill-22"
+            className="consumer-reports-logo__st3"
             d="M122.1 33.9l-.3-.9c-.6.4-1.2.6-2 .6-1 0-1.5-.6-1.5-1.7v-6.8h3.7V24h-3.7v-3.6h-1.2v11.5c0 1.7.9 2.8 2.6 2.8.9-.1 1.7-.4 2.4-.8"
           />
           <path
-            id="prefix__Fill-24"
-            className="prefix__st3"
+            id="consumer-reports-logo__Fill-24"
+            className="consumer-reports-logo__st3"
             d="M111.3 34.5v-6.2c.2-2.3 2.2-3.5 4.2-3.2v-1.2c-1.8-.3-3.4.6-4.2 2.2V24h-1.1v10.6h1.1z"
           />
           <path
-            id="prefix__Fill-26"
-            className="prefix__st3"
+            id="consumer-reports-logo__Fill-26"
+            className="consumer-reports-logo__st3"
             d="M103 33.4c-2.3 0-4.2-1.9-4.2-4.3s1.9-4.3 4.2-4.3c2.4 0 4.3 1.9 4.3 4.3s-1.9 4.3-4.3 4.3m0-9.6c-3 0-5.4 2.4-5.4 5.4s2.4 5.4 5.4 5.4c3 0 5.4-2.4 5.4-5.4s-2.4-5.4-5.4-5.4"
           />
           <path
-            id="prefix__Fill-28"
-            className="prefix__st3"
+            id="consumer-reports-logo__Fill-28"
+            className="consumer-reports-logo__st3"
             d="M90.8 33.4c-2.3 0-4.2-1.9-4.2-4.3s1.9-4.3 4.2-4.3c2.3 0 4.2 1.9 4.2 4.3s-1.9 4.3-4.2 4.3m0-9.6c-1.8 0-3.3 1-4.2 2.4v-2.3h-1.1v14.9h1.1v-6.7c.9 1.5 2.5 2.5 4.3 2.5 2.9 0 5.3-2.4 5.3-5.4s-2.4-5.4-5.4-5.4"
           />
           <path
-            id="prefix__Fill-30"
-            className="prefix__st3"
+            id="consumer-reports-logo__Fill-30"
+            className="consumer-reports-logo__st3"
             d="M75 28.4c.3-2.1 1.9-3.6 3.9-3.6 1.9 0 3.5 1.4 3.7 3.6H75zm8.7.3c0-3-2.2-4.9-4.8-4.9-2.8 0-5.1 2.2-5.1 5.4 0 3.1 2.4 5.4 5.3 5.4 1.7 0 3.2-.7 4.2-2l-.8-.7c-.8 1.1-1.9 1.6-3.3 1.6-2.2 0-3.9-1.7-4.1-4h8.7v-.8z"
           />
           <path
-            id="prefix__Fill-32"
-            className="prefix__st3"
+            id="consumer-reports-logo__Fill-32"
+            className="consumer-reports-logo__st3"
             d="M64.2 27v-6.8h3.9c2.4 0 3.8 1.4 3.8 3.4 0 1.8-1.2 3.4-3.9 3.4h-3.8zm4.9 1c2.6-.4 4.1-2.3 4.1-4.5 0-2.6-1.8-4.5-5-4.5H63v15.4h1.2v-6.3h3.4l4.4 6.3h1.5L69.1 28z"
           />
         </g>
