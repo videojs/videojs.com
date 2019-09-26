4969e1cc3d070214f124282e879d4cc9ef59a9fd
Matthew McClure <m@mmcc.io>
2019-09-26 16:37:50 -0700

    updated to a real 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,13 +1,33 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import Layout from '../components/Layout';
 import SEO from '../components/SEO';
+import Hero from '../components/Hero';
+import Container from '../components/Container';
+import { H1 } from '../components/Typography';
+import { theme } from '../utils/styles';
+
+const StyledHero = styled(Hero)`
+  ${({ theme }) => theme.media.xlLarge`
+    background-position: center -670px;
+
+    ${Container} {
+      padding-top: 12em;
+      padding-bottom: 12em;
+    }
+  `}
+`;
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <SEO title="Video.js - 404: Not found" />
+    <StyledHero theme={{ ...theme, currentTheme: theme.forest }}>
+      <Container>
+        <H1>Uh oh...Not found!</H1>
+        <p>We've looked everywhere, but this page was nowhere to be found.</p>
+      </Container>
+    </StyledHero>
   </Layout>
 );
 
