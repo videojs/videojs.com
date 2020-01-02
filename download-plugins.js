08ceef19e134921b6b56da5f717ec36a459a4cc1
Gary Katsevman <git@gkatsev.com>
2020-01-02 15:08:01 -0500

    pagination and download stats

--- download-plugins.js
+++ download-plugins.js
@@ -2,13 +2,11 @@ const https = require('https');
 const fs = require('fs');
 const Promise = require('bluebird');
 
-const url = new URL('https://registry.npmjs.org/-/v1/search?text=keywords:videojs-plugin,videojs-skin&size=100');
+const pluginsUrl = new URL('https://registry.npmjs.org/-/v1/search?text=keywords:videojs-plugin,videojs-skin&size=250');
 
-function getPlugins(from) {
+function get(url) {
   return new Promise(function(fulfill, reject) {
-    url.searchParams.set('from', from);
 
-    console.log(url.href);
     https.get(url.href, (res) => {
       const { statusCode } = res;
       const contentType = res.headers['content-type'];
@@ -47,11 +45,29 @@ function getPlugins(from) {
   });
 }
 
+function getPlugins(from) {
+  pluginsUrl.searchParams.set('from', from);
+  return get(pluginsUrl);
+}
+
+function getDownloadCount(package) {
+  const downloadsUrl = new URL(`https://api.npmjs.org/downloads/point/last-week/${package.name}`);
+  return get(downloadsUrl)
+  .then(function(download) {
+    package.downloads = download.downloads;
+    return package;
+  })
+  .catch(function(e) {
+    console.log(e);
+    return package;
+  });
+}
+
 let total = 0;
 let packages = [];
 getPlugins(0)
 .then(function(data) {
-  console.log(data.objects.length, data.total);
+  // set the total once
   total = data.total;
 
   return data;
@@ -59,18 +75,18 @@ getPlugins(0)
 .then(function getPluginsLoop(data) {
   packages.push.apply(packages, data.objects.map(data => data.package));
 
-  console.log(packages.length, total);
-
+  // if we aren't at the total yet, wait a bit, and get the next set of  items
   if (packages.length < total) {
-    return Promise.delay(1000).then(getPlugins(packages.length)).then(getPluginsLoop);
+    return Promise.delay(200).then(() => getPlugins(packages.length).then(getPluginsLoop));
   }
 
   return packages;
-  // parsedData = parsedData.objects.map(data => data.package);
-  //
-  // const dir = __dirname + '/src/data/plugins.json';
-  // fs.writeFileSync(dir, JSON.stringify(parsedData, null, 2));
 })
 .then(function(packages) {
-  console.log(packages.length);
+  // get the download counts for each item
+  return Promise.mapSeries(packages, getDownloadCount);
+})
+.then(function(packages) {
+  const dir = __dirname + '/src/data/plugins.json';
+  fs.writeFileSync(dir, JSON.stringify(packages, null, 2));
 });
