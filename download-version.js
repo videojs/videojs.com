7e3179b9a218847e82dc2533e9152446e20adf11
Gary Katsevman <git@gkatsev.com>
2020-01-03 12:34:52 -0500

    feat: get video.js version dynamically (#115)

---
+++ download-version.js
@@ -0,0 +1,40 @@
+const https = require('https');
+const fs = require('fs');
+
+https.get('https://registry.npmjs.org/video.js', (res) => {
+  const { statusCode } = res;
+  const contentType = res.headers['content-type'];
+
+  let error;
+  if (statusCode !== 200) {
+    error = new Error('Request Failed.\n' +
+                      `Status Code: ${statusCode}`);
+  } else if (!/^application\/json/.test(contentType)) {
+    error = new Error('Invalid content-type.\n' +
+                      `Expected application/json but received ${contentType}`);
+  }
+  if (error) {
+    console.error(error.message);
+    // Consume response data to free up memory
+    res.resume();
+    return;
+  }
+
+  res.setEncoding('utf8');
+  let rawData = '';
+  res.on('data', (chunk) => { rawData += chunk; });
+  res.on('end', () => {
+    try {
+      let parsedData = JSON.parse(rawData);
+
+      console.log(parsedData['dist-tags'].latest);
+      fs.writeFileSync(
+        __dirname + '/src/data/videojs-version.json',
+        JSON.stringify({version: parsedData['dist-tags'].latest}, null, 2));
+    } catch (e) {
+      console.error(e.message);
+    }
+  });
+}).on('error', (e) => {
+  console.error(`Got error: ${e.message}`);
+});
